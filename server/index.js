9de2e090de0c5cce427f8da85c315c1bf60b788c
diego <diegohuarcayarojas@gmail.com>
2022-05-05 11:42:48 -0500

    make it better

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ import express from "express";
 import { PORT } from "./config/config";
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("hola");
+});
+
 app.listen(PORT, function () {
   console.log("server on port ", PORT);
 });
